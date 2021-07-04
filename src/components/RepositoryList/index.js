cbe76401c865257f9df3a51a17ba0d985279a6a4
Wagner Barboza <wagnerbarboza@Wagners-MacBook-Pro.local>
2021-07-04 04:00:24 -0300

    feat: criamos a chamada para a api do repo no github

--- src/components/RepositoryList/index.js
+++ src/components/RepositoryList/index.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import RepositoryItem from "../RepositoryItem";
 
 import "./styles.scss";
@@ -8,6 +9,14 @@ const RepositoryList = () => {
     description: "Forms in React",
     link: "https://github.com/unform/unform",
   };
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/orgs/rocketseat/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
 
   return (
     <section className="repository-list">
---
+++ src/server/api.js
@@ -0,0 +1,1 @@
+const API_ROCKET = "https://api.github/orgs/rocketseat/repos";
